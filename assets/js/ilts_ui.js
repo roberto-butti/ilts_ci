c292d94bf32077f426eea936866e372b505757dc
Roberto B <roberto.butti@gmail.com>
2011-09-20 16:27:54 +0200

    aggiunto javascript per gestione user interface

---
+++ assets/js/ilts_ui.js
@@ -0,0 +1,14 @@
+/*
+$(function() {
+    $( "#playlist-selectable" ).selectable({
+      stop: function() {
+        var result = $( "#select-result" ).empty();
+        $( ".ui-selected", this ).each(function() {
+          var index = $( "#selectable li" ).index( this );
+          
+          /*result.append( " #" + ( index + 1 ) );*/
+        });
+      }
+    });
+  });
+  */
